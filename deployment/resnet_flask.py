1f25bdb0bebe3ada70774f51568805370fca232a
khazarkhorrami <khazar.khorrami@gmail.com>
2024-06-17 10:51:09 +0300

    testing env for resnet

--- deployment/resnet_flask.py
+++ deployment/resnet_flask.py
@@ -3,7 +3,16 @@ Created on Mon Jun 17 10:12:11 2024
 @author: Khazar Khorrami
 
 """
+# To install flask do this: conda install anaconda::flask
+from flask import Flask, redirect, url_for, request, render_template
+# To install keras do this: conda install anaconda::keras
+from tensorflow.keras.applications import resnet50
 
-from flask import Flask
 
-# ti install flask use: conda install anaconda::flask
+
+# Define a flask app
+app = Flask(__name__)
+# Define the model
+model = "../../models/ResNet50.h5" # model path (model_resnet.h5)
+model = resnet50() # this produces an error ...> fix the env and call REsNet50 instead 
+
